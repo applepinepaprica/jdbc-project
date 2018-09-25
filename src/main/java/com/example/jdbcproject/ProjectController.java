80d2f07fc735bb8a485c1c9e97b379b78b75abd0
applepinepaprica <applepinepaprica@gmail.com>
2018-09-25 20:36:34 +0300

    Fixing dots

--- src/main/java/com/example/jdbcproject/ProjectController.java
+++ src/main/java/com/example/jdbcproject/ProjectController.java
@@ -1,10 +1,10 @@
 package com.example.jdbcproject;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,7 +14,7 @@ public class ProjectController {
 	LineService lineService;
 
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public String home(@RequestParam(required = false)Integer id, 
+	public String home(@RequestParam(required = false)Integer id,
 				@RequestParam(required = false) String value, 
 				Model model) {
 		model.addAttribute("line", new Line());
