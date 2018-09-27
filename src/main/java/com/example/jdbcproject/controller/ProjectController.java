eae480b94c7e39b9b7b0e8819f56dde3d5ef83fe
applepinepaprica <applepinepaprica@gmail.com>
2018-09-27 17:09:49 +0300

    Added checkbox

--- src/main/java/com/example/jdbcproject/controller/ProjectController.java
+++ src/main/java/com/example/jdbcproject/controller/ProjectController.java
@@ -5,9 +5,9 @@ import com.example.jdbcproject.service.LineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ProjectController {
@@ -41,5 +41,14 @@ public class ProjectController {
 	public String addLinePost(Line line, Model model) {
 		lineService.insertLine(line);
 		return "redirect:/";
-	}	
+	}
+
+	@RequestMapping(value = { "/delete_checked" }, method = RequestMethod.POST)
+	public String deleteLines(@RequestBody List<Line> lines, Model model) {
+		for (Line l : lines) {
+			System.out.println(l.getId());
+			System.out.println(l.getValue());
+		}
+		return "redirect:/";
+	}
 }
---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('#delete_button').click(function() {
+
+        var data = [];
+        $('#lines').find('tr').each(function () {
+            if ($(this).find('input[type="checkbox"]').is(':checked')) {
+                var line = {
+                    id: $(this).find('td:eq(1)').text(),
+                    value: $(this).find('td:eq(2)').text()
+                };
+
+                data.push(line);
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: '/delete_checked',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            timeout: 600000
+        });
+    });
+});
